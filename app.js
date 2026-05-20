0db78d5ab29227d6a32064d47f41fc380610f97d
mhfanany-alt <mhfanany@gmail.com>
2026-05-20 17:49:08 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const db = require('./db.js');
+const port = 5775;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.get("/status", (req, res) => {
+    res.send(
+        '{"kode":"01", "status":"API Berbasis ExpressJS OK"}'
+    );
+})
+
+app.listen(port, () => {
+    console.log(`API berjalan di port ${port}`);
+})
+
+app.post("/backup", async (req, res) => {
+    let pesanx, kodex;
+    let nama = req.body.nama_backup;
+    let dtx = Buffer.from(req.body.dtx, 'base64').toString('utf-8');
+    let id = Date.now();
+    let arr_data = dtx.split("#");
+    let proses = await db.tambahBackup(id, nama, "nodejs");
+    if(proses == "1"){
+        let berhasil = 0;
+        let gagal = 0;
+        for(let k of arr_data){
+            if (k == "") continue;
+            let arr_data2 = k.split("|");
+            let idx = arr_data2[0];
+            let deskripsix = arr_data2[1];
+            let waktux = arr_data2[2];
+            let nominalx = arr_data2[3];
+            let jenisx = arr_data2[4];
+            let proses2 = await db.tambahTransaksi(`${id}-${idx}`, id, waktux, nominalx, jenisx, deskripsix);
+            proses2 == "1" ? berhasil++ : gagal++;
+        
+        }
+        pesanx = {kode: "01", status: "Proses Backup Berhasil Dengan Rincian", berhasil: berhasil, gagal: gagal};
+        kodex = 200;
+    }else{
+        pesanx = {kode: "00", status: "Proses Backup Gagal, Periksa Kembali Data Anda"};
+        kodex = 500;
+    }
+    return res.status(kodex).json(pesanx);
+})
---
+++ db.js
@@ -0,0 +1,31 @@
+const mysql = require('mysql2/promise');
+let sql;
+const buatKoneksi = async () => {
+    return await mysql.createConnection({
+        host: 'localhost',
+        user: 'u3cfe03y_hilal',
+        password: 'QQSVGFay6m7_Jms',
+        database: 'u3cfe03y_transaksi'
+    })
+}
+const tambahBackup = async (id, nama, channel) => {
+    const db = await buatKoneksi();
+    sql = `INSERT INTO backup VALUES('${id}', '${nama}', '${channel}',NOW())`;
+    try{
+        await db.execute(sql);
+        return "1";
+    }catch(err){
+        return "0";
+    }
+}
+const tambahTransaksi = async (idx, id, waktux, nominalx, jenisx, deskripsix) => {
+    const db = await buatKoneksi();
+    sql = `INSERT INTO backup_transaksi VALUES('${idx}', '${id}', '${waktux}', '${nominalx}', '${jenisx}', '${deskripsix}')`;
+    try{
+        await db.execute(sql);
+        return "1";
+    }catch(err){
+        return "0";
+    }
+}
+module.exports = {buatKoneksi, tambahBackup, tambahTransaksi};
