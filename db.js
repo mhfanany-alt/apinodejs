c487a647aaa9526cac254a076dd70af4b4badc5f
mhfanany-alt <mhfanany@gmail.com>
2026-06-06 15:34:04 +0700

    Update db.js

--- db.js
+++ db.js
@@ -12,7 +12,7 @@ const buatKoneksi = async () => {
 
 const bacaDetailBackup = async (id_backup) => {
     const db = await buatKoneksi();
-    sql = `SELECT * FROM backup_transaksi WHERE id_backup = '${id_backup}'ORDER BY tgl_jam`;
+    sql = `SELECT * FROM backup_transaksi WHERE id_backup = '${id_backup}' ORDER BY tgl_jam`;
     const [rows] = await db.execute(sql);
     return rows.length > 0 ? rows : false;
 }
