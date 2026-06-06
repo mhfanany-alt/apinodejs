c53e5ffd155b1f466292f17c1c42fdd47ea616a7
mhfanany-alt <mhfanany@gmail.com>
2026-06-06 15:21:19 +0700

    Update app.js

--- app.js
+++ app.js
@@ -17,6 +17,16 @@ app.listen(port, () => {
     console.log(`API berjalan di port ${port}`);
 })
 
+app.post("/detail_backup", async (req, res) => {
+    let idbackup = req.body.idbackup;
+    const dtdetail = await db.bacaDetailBackup(idbackup);
+    if(dtdetail == false){
+        res.send('{"kode":"00","pesan":"Data Detail Backup Tidak DiTemukan"}');
+    }else{
+        res.send('{"kode":"01","pesan":"Data Detail Backup DiTemukan","data":' + JSON.stringify(dtdetail) + '}');
+    }
+})
+
 app.get("/daftar_backup", async (req, res) => {
     const dtbackup = await db.bacaBackup();
     if(dtbackup == false){
